3b55912985df5c6e241bae491679365b812d8a71
Isaac Daniels <isaackice@gmail.com>
2023-03-29 14:19:27 -0500

    added basic animation

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const container = document.querySelector(".container");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (event) => {
-  let xAxis = (window.innerWidth / 2 - event.pageX) / 25;
-  let yAxis = (window.innerHeight / 2 - event.pageY) / 25;
-//   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  let xAxis = (window.innerWidth / 2 - event.pageX) / 30;
+  let yAxis = (window.innerHeight / 2 - event.pageY) / 30;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
   //Allows you to see the movement of the mouse
   console.log(xAxis, yAxis);
+});
+//Animate In
+container.addEventListener("mouseenter", (event) => {
+  card.style.transition = "none";
+});
+
+//Animate Out
+container.addEventListener("mouseleave", (event) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
